94e4b486eeca2fc7477cf7d337a44714603491ec
Yusuph <yusuph.mka@outlook.com>
2019-11-28 00:53:00 -0800

    Added option to print out all of the attempts

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ class Menu:
     def __init__(self):
         self.menu_options = {'1': "Create a new problem",
                         '2': "Add a new solution attempt",
+                        '3': "Review all existing attempts",
                         '0': "Exit"}
         self.introduction = "Welcome to Kuna Matata. It's Swahili for 'there are problems'.\
             \nAnd indeed, there are problems that need solving. Use this to track all of the\
@@ -20,13 +21,13 @@ class Menu:
         allowed_options = []
         for k,v in self.menu_options.items():
             if not self.problem:
-                if k in ['2']:
+                if k in ['2', '3']:
                     continue
                 allowed_options.append(k)
                 print(k, '\t', v)
             else:
                 if k in ['1']:
-                    print(f'\nCurrently working on: {self.problem.prob}')
+                    print(f'Currently working on: {self.problem.prob}')
                     continue
                 allowed_options.append(k)
                 print(k, '\t', v)
@@ -45,6 +46,7 @@ class Menu:
     def handle_option(self, opt, allowed_options):
         handle = {'1': lambda: self.create_new_problem(),
                  '2': lambda: self.add_solution_attempt(),
+                 '3': lambda: self.print_attempts(),
                  '0': sys.exit}
         if opt not in allowed_options:
             print(f"\nSelected option, {opt}, not allowed. Try again.")
@@ -57,8 +59,19 @@ class Menu:
         self.print_menu()
 
     def add_solution_attempt(self):
+        #TODO: Need a way to get back to menu in case I didn't mean to add a new solution
         self.problem.attempts.append(input("\nDetail the latest attempt: "))
         print(self.problem.attempts)
+        print()
+        self.print_menu()
+    
+    def print_attempts(self):
+        if self.problem.attempts:
+            for n, attempt in enumerate(self.problem.attempts):
+                print(f'Attempt #{n+1}', '\t', attempt, '\n')
+        else:
+            print("Every journey begins with the first step, and ya ain't done that yet!\n")
+        
         self.print_menu()
     
 
